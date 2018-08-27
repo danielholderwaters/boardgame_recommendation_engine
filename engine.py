519213298eacb29320d9571a6b5fd2452da333ef
Daniel Waters <42608070+danielholderwaters@users.noreply.github.com>
2018-08-27 16:33:41 -0400

    comments to fix an installation bug

--- engine.py
+++ engine.py
@@ -1,5 +1,10 @@
 #this is a recommendation engine project using boardgamegeek's API.
 #check this one out: https://apps.quanticfoundry.com/recommendations/tabletop/boardgame/
 #here is the preferred boardgamegeek python API package https://github.com/lcosmin/boardgamegeek/blob/develop/boardgamegeek/main.py
+import os
+os.chdir('/mnt/c/Users/danie/Desktop/PythonCode')
+print ("current working directory is:", os.getcwd())
 
+#i dont know why, but after creating a virtual environment, this command prompt syntax works in linux:
+#sudo python -m pip install boardgamegeek
 import boardgamegeek
