a927465c64cfb028c8836853f351de3f21534d3a
Daniel Waters <42608070+danielholderwaters@users.noreply.github.com>
2018-08-27 16:23:23 -0400

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,5 @@
+#this is a recommendation engine project using boardgamegeek's API.
+#check this one out: https://apps.quanticfoundry.com/recommendations/tabletop/boardgame/
+#here is the preferred boardgamegeek python API package https://github.com/lcosmin/boardgamegeek/blob/develop/boardgamegeek/main.py
+
+import boardgamegeek
